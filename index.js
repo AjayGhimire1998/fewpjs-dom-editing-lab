369f43dae27ff71cf9f8a4d7c7cf0a56b515c9c8
AjayGhimire <ajayghimire1998@gmail.com>
2022-03-05 14:58:19 +1100

    Done witht he lab

---
+++ index.js
@@ -0,0 +1,7 @@
+let body = document.createElement('body');
+document.appendChild(body);
+
+// const headingOne = document.createElement('h1');
+// body.appendChild(headingOne);
+ 
+document.querySelector('body').innerHTML = '<h1>My HTML adventure</h1>';
